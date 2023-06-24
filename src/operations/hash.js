50495c154ec23bbde1ee1981417c3088cf2e2cb4
petro1909 <petya-97_97@mail.ru>
2023-06-24 17:36:23 +0300

    feat: create function to calculate hash of input file

---
+++ src/operations/hash.js
@@ -0,0 +1,20 @@
+import crypto from "crypto";
+import fs from "fs/promises";
+import path from "path";
+import { getCurrentDirectory } from "../main.js";
+
+async function calculateHash(filePath) {
+  let hashedFileText;
+  const resultPath = path.resolve(getCurrentDirectory(), filePath);
+  try {
+     hashedFileText = await fs.readFile(resultPath);
+  } catch(err) {
+    throw new Error(`Hash operation failed: \n ${err}`);
+  }
+  let hash = crypto.createHash("sha256");
+  hash = hash.update(hashedFileText).digest("hex");
+  console.log(`hash: ${hash}`);
+  return hash;
+}
+const hashFunction = {"hash": calculateHash};
+export { hashFunction };
